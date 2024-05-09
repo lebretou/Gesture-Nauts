854ab73dc6ec6ce9af0fd16d6d7747db534f10d1
Leoxzz <leooooxzz@gmail.com>
2024-05-09 11:45:25 -0400

    f

--- web.py
+++ web.py
@@ -86,4 +86,4 @@ def toggle_processing():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
